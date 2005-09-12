47d16987d69f1a9628bcfb0f31a1c0a1bc215509
Jeff Connelly <sumi@xyzzy.cjb.net>
2005-09-12 09:27:19 +0000

    Exclude .svn

--- sumi/pkg.sh
+++ sumi/pkg.sh
@@ -4,7 +4,7 @@ RLS="0.8.14"
 rm *.pyc
 cd /home/jeff/p2p/
 echo -n "archiving " 
-tar -hvcf sumi-${RLS}.tar --exclude sumi/build --exclude sumi/dist --exclude sumi/incoming --exclude sumi/share --exclude sumi/otp --exclude sumi/c++-obsolete/sender --exclude sumi/c++-obsolete/sumiserv --exclude sumi/hash/mddriver --exclude sumi/hash/test --exclude sumi/rawproxd --exclude sumi/\*.pyc --exclude sumi/build\* --exclude sumi/dist\* --exclude sumi/\*.exe --exclude \*CVS\* sumi/*
+tar -hvcf sumi-${RLS}.tar --exclude sumi/build --exclude sumi/dist --exclude sumi/incoming --exclude sumi/share --exclude sumi/otp --exclude sumi/c++-obsolete/sender --exclude sumi/c++-obsolete/sumiserv --exclude sumi/hash/mddriver --exclude sumi/hash/test --exclude sumi/rawproxd --exclude sumi/\*.pyc --exclude sumi/build\* --exclude sumi/dist\* --exclude sumi/\*.exe --exclude \*.svn\* sumi/*
 
 echo -n "compressing "
 bzip2 -f sumi-${RLS}.tar
