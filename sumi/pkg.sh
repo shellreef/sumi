7c903695a05ae71b24566e710295f51993bd871c
Jeff Connelly <sumi@xyzzy.cjb.net>
2005-09-25 03:32:45 +0000

    Update Verizon

--- sumi/pkg.sh
+++ sumi/pkg.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-RLS="0.8.14"
+RLS="0.8.15"
 
 rm *.pyc
 cd /home/jeff/p2p/
 echo -n "archiving " 
-tar -hvcf sumi-${RLS}.tar --exclude sumi/build --exclude sumi/dist --exclude sumi/incoming --exclude sumi/share --exclude sumi/otp --exclude sumi/c++-obsolete/sender --exclude sumi/c++-obsolete/sumiserv --exclude sumi/hash/mddriver --exclude sumi/hash/test --exclude sumi/rawproxd --exclude sumi/\*.pyc --exclude sumi/build\* --exclude sumi/dist\* --exclude sumi/\*.exe --exclude \*.svn\* sumi/*
+tar -hvcf sumi-${RLS}.tar --exclude sumi/build --exclude sumi/dist --exclude sumi/incoming --exclude sumi/share --exclude sumi/otp --exclude sumi/c++-obsolete/sender --exclude sumi/c++-obsolete/sumiserv --exclude sumi/hash/mddriver --exclude sumi/hash/test --exclude sumi/rawproxd --exclude sumi/\*.pyc --exclude sumi/build\* --exclude sumi/dist\* --exclude sumi/\*.exe --exclude \*.svn\* --exclude \*.exe --exclude sumi/sumi-repos.gz --exclude doc/*.sql --exclude *.sql --exclude doc/wiki.xml.gz --exclude *.tmp sumi/*
 
 echo -n "compressing "
 bzip2 -f sumi-${RLS}.tar
